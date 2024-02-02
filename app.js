2688f6a40e683bb9c4440bc92c3662e9e776bb1b
Michel Alves <82483090+michelssalves@users.noreply.github.com>
2024-02-02 15:50:08 -0300

    organizando grid css

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            playerLife: 10,
+            monsterLife: 10,
+        
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    watch:{
+
+    }
+
+})
